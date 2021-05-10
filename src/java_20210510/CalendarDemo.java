b48dda5d3c1d265da3d1a7d7b4758d4fc3fa8696
CHOHEEJEONG <chohee0505@gmail.com>
2021-05-10 17:51:58 +0900

    2021-05-10-v3

---
+++ src/java_20210510/CalendarDemo.java
@@ -0,0 +1,11 @@
+package java_20210510;
+
+import java_20210507.Calendar;
+
+public class CalendarDemo {
+	
+	public static void main(String[] args) {
+		Calendar c = new Calendar(2021,05,10);
+		c.print();
+	}
+}
